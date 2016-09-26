c4c85cb20d4df19af277b1238d947bfc5e03e2c3
Michael Menzel <mugglmenzel@gmail.com>
2016-09-26 14:24:26 +0200

    infrastructure user data scripts for ec2 instances

---
+++ infrastructure/setupLargeProducer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+sudo apt-get install -y git
+
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update -y
+echo "debconf shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+echo "debconf shared/accepted-oracle-license-v1-1 seen true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update -y
+sudo apt-get install -y sbt
+
+bash aws-kinesis-experiments/kinesis-stream-producer/run45.sh
