6a7051434de74b54db0d587c3f894f5cf89d197f
Michael Menzel <mugglmenzel@gmail.com>
2016-09-26 11:21:53 +0200

    Added CLI options and validation (with scopt)

--- kinesis-stream-producer/build.sbt
+++ kinesis-stream-producer/build.sbt
@@ -5,5 +5,6 @@ version := "1.0"
 scalaVersion := "2.11.5"
 
 libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.11.37"
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.5.0"
 
     
--- kinesis-stream-producer/src/main/scala/DataProducerApp.scala
+++ kinesis-stream-producer/src/main/scala/DataProducerApp.scala
@@ -1,8 +1,24 @@
+import com.amazonaws.regions.Regions
+import scopt.OptionParser
+
 /**
   * Created by menzelmi on 26.09.16.
   */
 object DataProducerApp extends App {
 
-  TestDataProducer().printInfos.consumeAllShards.produce
+  implicit val regionsRead: scopt.Read[Regions] =
+    scopt.Read.reads(Regions fromName)
+
+  new OptionParser[TestDataProducer]("ktdp"){
+    head("Kinesis Test Data Prosumer (ktdp)", "1.0")
+    opt[String]('n', "stream-name").required().action((value, producer) => producer.copy(streamName = value))
+    opt[String]('d', "data").optional().action((value, producer) => producer.copy(dataSample = value))
+    opt[Regions]('r', "region").optional().action((value, producer) => producer.copy(region = value))
+    opt[Int]('t', "produce-throttle").optional().action((value, producer) => producer.copy(produceThrottle = value))
+  }.parse(args, TestDataProducer()) match {
+    case Some(producer) =>
+      producer.printInfos.consumeAllShards.produce
+    case None =>
+  }
 
 }
